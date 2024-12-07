21fe6631a8b00207e5326a00f266a0f1d650f14c
joshuatuan <itsjoshuatuan+git@gmail.com>
2024-12-07 23:19:01 +0800

    added constant folder for static data

---
+++ app/constants/socialLinks.ts
@@ -0,0 +1,24 @@
+import { Facebook, Github, Linkedin, Mail } from "lucide-react";
+
+export const socialLinks = [
+  {
+    icon: Linkedin,
+    link: "https://www.linkedin.com/in/joshua-miguel-tuan-21861b26a/",
+    label: "LinkedIn",
+  },
+  {
+    icon: Mail,
+    link: "mailto:itsjoshuatuan@gmail.com",
+    label: "Email",
+  },
+  {
+    icon: Github,
+    link: "https://github.com/joshuatuan",
+    label: "GitHub",
+  },
+  {
+    icon: Facebook,
+    link: "https://www.facebook.com/joshuamigueI/",
+    label: "Facebook",
+  },
+];
