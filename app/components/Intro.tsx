c79bbdaa7c7f73185d13f78d1777ac9027ef3d6f
joshuatuan <itsjoshuatuan+git@gmail.com>
2024-12-07 23:26:38 +0800

    improved styling

--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -27,7 +27,7 @@ export default function Intro({ introData }: IntroProps) {
             <p className="mb-2 text-2xl font-[450] md:mb-4 lg:text-3xl">
               Hi there 👋 I am
             </p>
-            <h1 className="mb-8 text-4xl font-medium md:mb-6 lg:mb-9 lg:text-5xl xl:text-6xl">
+            <h1 className="mb-8 text-4xl font-bold md:mb-6 lg:mb-9 lg:text-5xl xl:text-6xl">
               Joshua Miguel Tuan
             </h1>
             <p className="text-xl font-[450] leading-relaxed text-stone-800 md:max-w-2xl lg:px-0 xl:text-2xl xl:leading-loose">
