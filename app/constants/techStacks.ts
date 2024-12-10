fd095299f69e144f0e93e72d8afe2055f198769e
joshuatuan <itsjoshuatuan+git@gmail.com>
2024-12-10 20:54:13 +0800

    organized constant data

---
+++ app/constants/techStacks.ts
@@ -0,0 +1,65 @@
+import {
+  SiGit,
+  SiJavascript,
+  SiNextdotjs,
+  SiNpm,
+  SiPython,
+  SiReact,
+  SiReactquery,
+  SiRedux,
+  SiSupabase,
+  SiTailwindcss,
+  SiTypescript,
+  SiVite,
+} from "@icons-pack/react-simple-icons";
+
+export const techStack = [
+  {
+    name: "React JS",
+    Icon: SiReact,
+  },
+  {
+    name: "Git",
+    Icon: SiGit,
+  },
+  {
+    name: "Supabase",
+    Icon: SiSupabase,
+  },
+  {
+    name: "Javascript",
+    Icon: SiJavascript,
+  },
+  {
+    name: "NPM",
+    Icon: SiNpm,
+  },
+  {
+    name: "Python",
+    Icon: SiPython,
+  },
+  {
+    name: "Tailwind CSS",
+    Icon: SiTailwindcss,
+  },
+  {
+    name: "React Query",
+    Icon: SiReactquery,
+  },
+  {
+    name: "Next.JS",
+    Icon: SiNextdotjs,
+  },
+  {
+    name: "Typescript",
+    Icon: SiTypescript,
+  },
+  {
+    name: "Vite",
+    Icon: SiVite,
+  },
+  {
+    name: "Redux",
+    Icon: SiRedux,
+  },
+];
